d0076b4f97e3700041ff61bb8a2025c7feb34471
Ingrid S. <110192683+ingridevv@users.noreply.github.com>
2025-07-21 20:27:45 -0300

    Add files via upload

---
+++ pratica-1/02_calculadora_de_soma.py
@@ -0,0 +1,13 @@
+"""
+2- Calculadora de Soma
+* Desenvolva um programa que soma dois números. 
+* Use as variáveis numero1 = 12 e numero2 = 14. 
+* O programa deve calcular a soma e exibir o resultado.
+
+"""
+
+def calcularSoma(a, b): 
+   return a + b
+
+resultado = calcularSoma(12, 14)
+print(resultado)
