9e52b19768b28e05c81e2e3d5ee4a41be2c8f9da
ingridevv <personnel.ingrid@gmail.com>
2025-07-24 22:00:36 -0300

    chore: adicionar comentários explicativos

--- pratica-4/02_registro_de_notas.py
+++ pratica-4/02_registro_de_notas.py
@@ -11,14 +11,17 @@ Desenvolva um programa para registrar notas de uma turma e calcular a média fin
 
 print("======================= Registro de Notas =======================")
 
+# Array que irá armazenar as notas computadas
 notas = []
 
+# Laço de repetição While que solicita continuamente as notas
 while True: 
     entrada = input("Digite a nota de um aluno (de 0 a 10) ou 'Fim' para encerrar: ")
     
     if entrada.lower() in ("fim", "sair"):
         break
-
+    
+    # Tratamento de exceção das entradas
     try:
         nota = int(entrada)
         if 0 <= nota <= 10:
@@ -28,6 +31,7 @@ while True:
     except ValueError:
         print("Entrada inválida! Digite um número inteiro de 0 a 10 ou 'Fim' para encerrar.")
 
+# Exibe as notas registradas
 print("Notas registradas:")
 for nota in notas:
     print(nota)
