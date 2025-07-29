d89787d70a5d28a7c395f3fce4477bc23513f3e0
ingridevv <personnel.ingrid@gmail.com>
2025-07-28 22:32:06 -0300

    fix: subir correcao exercicio 4

--- pratica-6/04_conversor_de_moedas.py
+++ pratica-6/04_conversor_de_moedas.py
@@ -11,12 +11,29 @@ Dica: A conversão da data/hora pode ser feita com o módulo `datetime`.
 
 """
 import requests
-import datetime
+from datetime import datetime
 
-url = requests.get("https://economia.awesomeapi.com.br/last/{moeda}-BRL")
+def exibir_cotacao(moeda):
+    moeda = moeda.upper() 
+    url = f"https://economia.awesomeapi.com.br/last/{moeda}-BRL"
+  
+    try: 
+        response = requests.get(url)
+        data = response.json()
 
+        chave = f"{moeda}BRL"
+        if chave not in data:
+            print("Código da moeda inválido.")
+            return
 
+        cotacao = data[chave]
+        valor_atual = cotacao["bid"]
+        valor_max = cotacao["high"]
+        valor_min = cotacao["low"]
+        data_hora = datetime.fromtimestamp(int(cotacao["timestamp"])).strftime('%d/%m/%Y %H:%M:%S')
+        print(f"{moeda}/BRL | Atual: R${valor_atual} | Máx: R${valor_max} | Mín: R${valor_min} | Atualizado em: {data_hora}")
+    except Exception as e:
+        print("Erro ao consultar API.", e)
 
-
-
-print(input("Qual o código da moeda que deseja converter? "))
+moeda = input("Qual o código da moeda que deseja converter (ex: USD, EUR, GBP)? ")
+exibir_cotacao(moeda)
