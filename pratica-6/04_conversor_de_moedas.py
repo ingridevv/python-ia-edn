f8917450fd9a892220b608a2d95845ec26aea307
ingridevv <personnel.ingrid@gmail.com>
2025-07-28 21:48:32 -0300

    feat: adicionar exercicio 04

---
+++ pratica-6/04_conversor_de_moedas.py
@@ -0,0 +1,22 @@
+"""
+4- Conversor de Moedas (para Reais - BRL)  
+Crie um programa que mostra a cotação atual de moedas estrangeiras em relação ao Real. O programa deve:
+
+* Solicitar ao usuário o código da moeda estrangeira (ex: USD, EUR, GBP).  
+* Acessar a API: "https://economia.awesomeapi.com.br/last/{moeda}-BRL".  
+* Exibir a cotação atual, o valor máximo, o valor mínimo e a data/hora da última atualização.  
+* Informar ao usuário se o código da moeda for inválido ou houver falha na conexão.  
+
+Dica: A conversão da data/hora pode ser feita com o módulo `datetime`.
+
+"""
+import requests
+import datetime
+
+url = requests.get("https://economia.awesomeapi.com.br/last/{moeda}-BRL")
+
+
+
+
+
+print(input("Qual o código da moeda que deseja converter? "))
